b3eb66869eb311de6952ad491eab78fcd3089fec
mimilatyu <dimatusyan@gmail.com>
2021-09-01 10:49:54 +0300

    added posters, modified buttons

--- app/src/main/java/com/example/kinoproisk/MainActivity.kt
+++ app/src/main/java/com/example/kinoproisk/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.kinoproisk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.view.View
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onClickToastMenu(view: View){
+        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onClickToastFavor(view: View){
+        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onClickToastLater(view: View){
+        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onClickToastMenuColl(view: View){
+        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onClickToastMenuSet(view: View){
+        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+    }
+
+
+
+
+
+
+
 }
