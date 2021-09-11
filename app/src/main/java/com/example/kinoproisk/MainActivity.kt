0f85678e7e02f3a723825ba2250e44fcb4d52309
mimilatyu <dimatusyan@gmail.com>
2021-09-11 18:40:02 +0300

    Added - icons, - bottom navigation, toolbar

--- app/src/main/java/com/example/kinoproisk/MainActivity.kt
+++ app/src/main/java/com/example/kinoproisk/MainActivity.kt
@@ -4,39 +4,58 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import android.view.View
+import com.example.kinoproisk.databinding.ActivityMainBinding
+
 
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initNavigation()
+
     }
 
-    fun onClickToastMenu(view: View){
-        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+    private fun initNavigation(){
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
-
-    fun onClickToastFavor(view: View){
-        Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-    }
-
-    fun onClickToastLater(view: View){
-        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
-    }
-
-    fun onClickToastMenuColl(view: View){
-        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-    }
-
-    fun onClickToastMenuSet(view: View){
-        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
-    }
-
-
-
-
-
 
 
 }
